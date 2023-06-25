ee504bf759f456dc67bee418c0cceff7a14cfb0a
Nguyễn văn Huy <93319143+Ca-mega3-151@users.noreply.github.com>
2023-06-25 16:31:18 +0000

    Update 4 filesgg

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from "react";
 
 function Dropdown() {
-  return ( 
+  return (
     <div className="dropdown">
-      <a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-       Ấn đi nào !
+      <a
+        className="btn btn-secondary dropdown-toggle"
+        href="#"
+        role="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Ấn đi nào !
       </a>
 
       <ul className="dropdown-menu">
-        <li><a className="dropdown-item" href="#">Picaso</a></li>
-        <li><a className="dropdown-item" href="#">Zicc</a></li>
-        <li><a className="dropdown-item" href="#">kaka</a></li>
+        <li>
+          <a className="dropdown-item" href="#">
+            Picaso
+          </a>
+        </li>
+        <li>
+          <a className="dropdown-item" href="#">
+            Zicc
+          </a>
+        </li>
+        <li>
+          <a className="dropdown-item" href="#">
+            kaka
+          </a>
+        </li>
       </ul>
     </div>
   );
 }
 
-export {  Dropdown}  
+export { Dropdown };
--- src/Offcanvas.jsx
+++ src/Offcanvas.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-
 function Offcanvas() {
   return (
     <div>
-      <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">OFFcanvas</button>
+      <button
+        className="btn btn-primary"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasWithBothOptions"
+        aria-controls="offcanvasWithBothOptions"
+      >
+        OFFcanvas
+      </button>
 
-      <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-        <div className="offcanvas-header"> 
-          <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Trang chủ </h5>
-          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      <div
+        className="offcanvas offcanvas-start"
+        data-bs-scroll="true"
+        tabIndex="-1"
+        id="offcanvasWithBothOptions"
+        aria-labelledby="offcanvasWithBothOptionsLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">
+            Trang chủ{" "}
+          </h5>
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
         </div>
         <div className="offcanvas-body">
           <p> Ăn Bánh trả tiền ăn tài trả nợ </p>
@@ -18,4 +37,4 @@ function Offcanvas() {
   );
 }
 
-export {Offcanvas} ;
+export { Offcanvas };
